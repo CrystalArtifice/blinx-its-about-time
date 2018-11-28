89e7b3c95c106204c0f9ef12956542e159689597
Scabbage <TheScabbage@gmail.com>
2018-11-28 19:09:53 +0800

    Made the camera follow restricted to Transform components only.

--- Blinx It's About Time/Assets/Scripts/CameraFollow.cs
+++ Blinx It's About Time/Assets/Scripts/CameraFollow.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     // The player to follow.
-    public GameObject player;
+    public Transform player;
 
     // The offset between the camera and the player's position.
     private Vector3 offset;
 
     void Start ()
     {
-        offset = transform.position - player.transform.position;
+        offset = transform.position - player.position;
     }
 
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.position + offset;
 	}
 }
