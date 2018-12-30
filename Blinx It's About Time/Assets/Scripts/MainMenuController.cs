ea48549760558f748ca59baebdf47d1134b9d111
Scabbage <TheScabbage@gmail.com>
2018-12-30 21:51:53 +0800

    Added basic main menu

---
+++ Blinx It's About Time/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+    public void CreateNewGame()
+    {
+        // TODO: Add game saves
+        // for now, just load the test level
+        SceneManager.LoadScene("TestArea");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
